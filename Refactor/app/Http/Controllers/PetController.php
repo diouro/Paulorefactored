bbb8f62535643ceca728911177215ca49270b9a0
Paulo Goncalves <phsgjc@gmail.com>
2018-10-16 23:03:31 +1100

    adding the mess to a route class and create controllers and models

---
+++ Refactor/app/Http/Controllers/PetController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PetController extends Controller
+{
+
+    public static function getPetById()
+    {
+     
+        
+    }
+    
+    public static function deletePet()
+    {
+     
+        
+    }
+
+}
---
+++ Refactor/app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserControllers extends Controller
+{
+
+    public static function getUsers()
+    {
+     
+        
+    }
+    
+    public static function getUsersById()
+    {
+     
+        
+    }
+    
+    public static function addUser()
+    {
+     
+        
+    }
+
+
+
+}
