873c5163199009453847231c451ba5ef5a2d26e1
Paulo Goncalves <phsgjc@gmail.com>
2018-10-17 12:38:27 +1100

    inject the custom fields to response

--- Laravel/app/User.php
+++ Laravel/app/User.php
@@ -17,6 +17,13 @@ class User extends Model
     // Hide password 
     protected $hidden = ['password'];
 
+    protected $appends = [
+        'display_name',
+        'login_date_formatted',
+        'create_date_formatted',
+        'update_date_formatted'
+    ];
+    
     /**
 	 * The attributes that are mass assignable.
 	 *
@@ -41,21 +48,34 @@ class User extends Model
      */
     public function getLoginDateFormattedAttribute()
     {
-        return $this->login_date->format('Y-m-d H:i:s');
+        if($this->login_date)
+        {
+            return $this->login_date->format('Y-m-d H:i:s');
+        }
+        return '';
     }
     /**
      * Function to accessor the user display name
      */
     public function getCreateDateFormattedAttribute()
     {
-        return $this->created_at->format('Y-m-d H:i:s');
+        if($this->created_at)
+        {
+            return $this->created_at->format('Y-m-d H:i:s');
+        }
+
+        return '';
     }
     /**
      * Function to accessor the user display name
      */
     public function getUpdateDateFormattedAttribute()
     {
-        return $this->updated_at->format('Y-m-d H:i:s');
+        if($this->updated_at)
+        {
+            return $this->updated_at->format('Y-m-d H:i:s');
+        }
+        return '';
     }
 
     /**
